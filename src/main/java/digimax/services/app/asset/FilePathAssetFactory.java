1a36d2a83bb9d74622a107d2164dda571c6d83b7
digimax <williams.jonathan@gmail.com>
2013-04-12 16:58:02 -0600

    starting work on FilePath assets handling.

---
+++ src/main/java/digimax/services/app/asset/FilePathAssetFactory.java
@@ -0,0 +1,22 @@
+package digimax.services.app.asset;
+
+import org.apache.tapestry5.Asset;
+import org.apache.tapestry5.ioc.Resource;
+import org.apache.tapestry5.services.AssetFactory;
+
+/**
+ * A product of Digimax Technology Inc. (digimax.com)
+ * User: jonwilliams
+ * Date: 4/12/13
+ * Time: 4:53 PM
+ */
+public class FilePathAssetFactory implements AssetFactory {
+
+    public Resource getRootResource() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Asset createAsset(Resource resource) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
