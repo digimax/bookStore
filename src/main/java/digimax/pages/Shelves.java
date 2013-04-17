57b54dc2b57e30e7606494330800adaf2ff02b06
digimax <williams.jonathan@gmail.com>
2013-04-17 17:13:13 -0600

    Simple change to help debugging missing page template source on Minty.

---
+++ src/main/java/digimax/pages/Shelves.java
@@ -0,0 +1,11 @@
+package digimax.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jon
+ * Date: 17/04/13
+ * Time: 5:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Shelves {
+}
