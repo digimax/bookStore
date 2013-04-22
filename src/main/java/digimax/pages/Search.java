403643d647ee6d2326898b08abb05d61ba7b57b0
digimax <williams.jonathan@gmail.com>
2013-04-21 21:12:56 -0600

    Progress on Search page. Added test page to help debug a multizone update problem.

--- src/main/java/digimax/pages/Search.java
+++ src/main/java/digimax/pages/Search.java
@@ -2,18 +2,25 @@ package digimax.pages;
 
 import digimax.entities.people.Author;
 import digimax.services.domain.PersonService;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.PersistenceConstants;
 import org.apache.tapestry5.alerts.AlertManager;
 import org.apache.tapestry5.alerts.Duration;
 import org.apache.tapestry5.alerts.Severity;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.beaneditor.Validate;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 import org.hibernate.sql.Insert;
 import org.slf4j.Logger;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,7 +32,7 @@ import java.util.List;
 public class Search {
 
     @Inject
-    Logger logger;
+    private Logger logger;
 
     @Inject
     private AlertManager manager;
@@ -34,30 +41,56 @@ public class Search {
     private Messages messages;
 
     @Inject
+    private Request request;
+
+    @Inject
+    private AjaxResponseRenderer ajaxResponseRenderer;
+
+    @Inject
     PersonService personService;
 
     @InjectComponent
     private Form byAuthorForm;
 
     @InjectComponent
+    private Zone byAuthorFormZone;
+
+    @InjectComponent
     private Form byTitleForm;
+
+    @InjectComponent
+    private Zone authorsZone;
 
     @Property
 //    @Validate("required")
-    private String author;
+    private String authorName;
 
     @Property
 //    @Validate("required")
     private String title;
 
+    @Property
+    private Author author;
+
+    @Property
+    @Persist(PersistenceConstants.SESSION)
+    private List<Author> authors;
+
     void onSelectedFromSearchByAuthor() {
+        authors = null;
         logger.debug("Start Diagnostics");
         if (byAuthorForm.getHasErrors()) {
             return;
         }
-        List<Author> authors = personService.findAuthors(author);
+        authors = personService.findAuthors(authorName);
         if (authors==null || authors.size()==0) {
-            manager.alert(Duration.SINGLE, Severity.WARN, messages.format("noAuthorsFound", author));
+            manager.alert(Duration.SINGLE, Severity.WARN, messages.format("noAuthorsFound", authorName));
+            return;
+        }
+        if (request.isXHR()) {
+            logger.debug("byAuthorFormZone :: {}", byAuthorFormZone);
+            logger.debug("authorsZone :: {}", authorsZone);
+            ajaxResponseRenderer.addRender(byAuthorFormZone).addRender(authorsZone);
         }
         logger.debug("End Diagnostics");
     }
@@ -68,6 +101,10 @@ public class Search {
             return;
         }
         logger.debug("End Diagnostics");
+    }
+
+    public boolean getHasAuthors() {
+        return (authors!=null && authors.size()>0);
     }
 
 }
